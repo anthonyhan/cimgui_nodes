4657088056ea93a4d3ea84f81d5e4103dcef8d51
Han Qian <qianhan1986@msn.com>
2023-10-09 16:37:40 +0800

    add generator script and template

---
+++ generator/cimgui_nodes_template.cpp
@@ -0,0 +1,9 @@
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "./nodes/nodes.h"
+#include "cimgui_nodes.h"
+
+
+
+#include "auto_funcs.cpp"
+
---
+++ generator/cimgui_nodes_template.h
@@ -0,0 +1,12 @@
+#ifndef CIMGUINODES_INCLUDED
+#define CIMGUINODES_INCLUDED
+
+#include "cimgui.h"
+
+#ifdef CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "imgui_structs.h"
+#else
+#endif // CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+
+#include "auto_funcs.h"
+#endif //CIMGUINODES_INCLUDED
